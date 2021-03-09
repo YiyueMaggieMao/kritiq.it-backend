67a787b95bc8522eaed98eeb54ef1ae04520090a
YiyueMaggieMao <myykitty711@gmail.com>
2021-03-09 23:23:04 +0800

    Added configs

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const express = require('express');
+const config = require('./config');
 
 const server = express();
 const router = express.Router();
-server.use("/", router); // Registers all routes starting with "/" to router
+server.use("/api", router); // Registers all routes starting with "/api" to router
 
 // The route that returns all the posts
 router.get('/posts', (req, res) => {
-    res.status(200).json({"author": "Stanley Lee"});
+    res.status(200).json([{"author": "Stanley Lee"}, {"author": "Jeffrey Ha"}]);
 });
 
 // Sets the server to start on port 5000
-server.listen(5000, () => {
-    console.log("Server started on port 5000");
+server.listen(config.PORT, () => {
+    console.log("Server started on port " + config.PORT);
 });
